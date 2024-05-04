2b7969135d688625b53970c06c0a1bdd720cbf77
duong1703 <dduong1703@gmail.com>
2024-05-04 12:34:20 +0700

    update

--- resources/views/Client/pages/product.blade.php
+++ resources/views/Client/pages/product.blade.php
@@ -13,7 +13,7 @@
 
                     <div class="price-range">
                         <!--price-range-->
-                        <h2>Price Range</h2>
+                        <h2>Lọc giá/h2>
                         <div class="well">
                             <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600"
                                 data-slider-step="5" data-slider-value="[250,450]" id="sl2"><br />
